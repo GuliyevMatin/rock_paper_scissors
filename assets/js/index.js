fc524271139a895eb8dbc213ed9a0a36475f21b3
Metin1323 <95915073+Metin-Quliyev@users.noreply.github.com>
2022-04-02 16:59:35 +0400

    created rps game

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,18 @@
 // $(()=> $('#myModal').modal('show'))
+
+// window.onkeypress = function(e) {
+//     alert("sadasd")
+// }
+let player = document.querySelector("#player");
+let playerChoose = document.querySelector("#playerChoose");
+let comp = document.querySelector("#comp");
+let compchoose = document.querySelector("#compchoose")
+
+
+let words = ["Rock", "Scissors", "Paper"];
+let p = document.createElement("p");
+let a = Math.floor(Math.random() * words.length);
+
+function choosePlayer(){
+    
+}
